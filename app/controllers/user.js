8e15fb81726b34b73b831689113b5872ab3406e1
Rajas1312 <you@example.com>
2021-03-17 13:07:56 +0530

    refactor : view.ejs file add style

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -14,7 +14,8 @@ const jwt = require('jsonwebtoken')
 const helper = require('../../utility/helper')
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
-const { error } = require('../../logger/logger.js');
+const ejs = require('ejs')
+const fs = require('fs')
 dotenv.config();
 
 const ControllerUserValidation = Joi.object().keys({
@@ -120,20 +121,27 @@ class UserController {
                         rejectUnauthorized: false
                     }
                 });
-                let mailOptions = {
-                    from: result.email,
-                    to: 'itsmerajas2@gmail.com',
-                    subject: 'Sending Email using Node.js',
-                    text: ` ${'http://localhost:3000/login'} ${result.token}`
 
-                };
-                transporter.sendMail(mailOptions, function (error, info) {
-                    if (error) {
-                        console.log(error);
+                ejs.renderFile('app/views/view.ejs', 'utf8', (err, data) => {
+                    if (err) {
+                        console.log('error', err)
                     } else {
-                        console.log('Email sent: ' + info.response);
+                        let mailOptions = {
+                            from: result.email,
+                            to: 'itsmerajas2@gmail.com',
+                            subject: 'Sending Email using Node.js',
+                            html: `${data}<p1> ${'http://localhost:3000/resetPassword'} ${result.token}</p1>`
+
+                        };
+                        transporter.sendMail(mailOptions, function (error, info) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                console.log('Email sent: ' + info.response);
+                            }
+                        });
                     }
-                });
+                })
             }
         })
     }
